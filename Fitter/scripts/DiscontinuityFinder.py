6bed2b59568ce532f2e62dc521119ac170ed92bc
Anthony Lefeld <alefeld@alumni.nd.edu>
2019-11-07 14:36:06 -0500

    Revert change of systematic names to line up with results for Nov 4 talk.

--- Fitter/scripts/DiscontinuityFinder.py
+++ Fitter/scripts/DiscontinuityFinder.py
@@ -7,8 +7,8 @@ parser.add_argument("basename", help="basename of fit file to extract discontinu
 parser.add_argument("poi", help="parameter to find discontinuities in")
 args = parser.parse_args()
 
-systematics = ['CERR1','CERR2','CMS_eff_em','CMS_scale_j','ChargeFlips','FR_FF','LEPID','Q2FR','PDF','PSISR','PU',
-               'QCDscale_V','QCDscale_VV','QCDscale_VVV','QCDscale_tHq','QCDscale_ttG','QCDscale_ttH','QCDscale_ttV',
+systematics = ['CERR1','CERR2','CMS_eff_em','CMS_scale_j','ChargeFlips','FR_FF','LEPID','MUFR','PDF','PSISR','PU',
+               'QCDscale_V','QCDscale_VV','QCDscale_VVV','QCDscale_tHq','QCDscale_ttG','QCDscale_ttH','QCDscale_ttbar',
                'hf','hfstats1','hfstats2','lf','lfstats1','lfstats2','lumi_13TeV_2017','pdf_gg','pdf_ggttH','pdf_qgtHq','pdf_qq',
               ]
 wcs = ['ctW','ctZ','ctp','cpQM','cbW','cpQ3','cptb','cpt','cQl3i','cQlMi','cQei','ctli','ctei','ctlSi','ctlTi']
